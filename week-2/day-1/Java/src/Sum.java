f28a3f359089b6215a46d11554f23873f58b136d
CheeseCake <decsei.david@gmail.com>
2019-12-09 17:34:10 +0100

    added some more (append, colors, doubles, sumall, swap)

--- week-2/day-1/Java/src/Sum.java
+++ week-2/day-1/Java/src/Sum.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Sum {
+
 // Create the usual class wrapper and main method on your own.
 
 // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
